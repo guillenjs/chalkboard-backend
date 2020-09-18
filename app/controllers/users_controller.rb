f832b1694c365b36609aac84484031c4570d49e7
guillenjs <jorgeguilln@gmail.com>
2020-09-18 16:52:36 -0400

    set up authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    before_action :authorized, only: [:auto_login]
+
+#register
+    def create
+        @user = User.create(userParams)
+        if @user.valid?
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {error: "Invalid username or password"}
+        end
+      end
+    
+#logging in
+    def login
+            @user = User.find_by(username: params[:username])
+        
+            if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+            else
+            render json: {error: "Invalid username or password"}
+            end
+     end
+
+     def userParams 
+        params.permit(:username, :password, :age)
+     end
+end
