f38d72973d60c7bb85917efba57c05b632cf9da3
guillenjs <jorgeguilln@gmail.com>
2020-09-27 22:17:13 -0400

    solved some grade table issues

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,2 +1,19 @@
 class GradesController < ApplicationController
+    def index 
+        grades = Grade.all 
+
+        render json: grades
+    end
+
+    def create
+        grade = Grade.create(gradeParams)
+
+        render json: grade
+    end
+
+    private
+
+    def gradeParams
+        params.permit(:grade, :user_id, :teacher_id, :assignment_id)
+    end
 end
